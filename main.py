76eab1f84062c32a7a0d60d917b64496551b9de2
s214272 <s214272@dtu.dk>
2024-09-09 14:43:14 +0200

    Sådan der du

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import ifcopenshell
 
 #from .rules import windowRule
@@ -7,12 +8,25 @@ import ifcopenshell
 #model = ifcopenshell.open(r'C:\Users\thoma\Documents\GitHub\AdvBIM-G25\models\CES_BLD_24_06_STR.ifc')
 
 #Coloured model:
-model = ifcopenshell.open(r'C:\Users\thoma\Documents\GitHub\AdvBIM-G25\models\LLYN - STRU.ifc')
+# Få den nuværende sti til 'main.py'
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Find stien til 'models'-mappen
+models_directory = os.path.join(current_directory, 'models')
+
+# Filnavnet på modellen
+model_filename = 'LLYN - STRU.ifc'
+
+# Saml hele stien til IFC-filen
+model_path = os.path.join(models_directory, model_filename)
+
+# Åbn modellen
+model = ifcopenshell.open(model_path)
 
 print(model)
 
+# Eksempel på at få IfcBeam elementer
 things = model.by_type('IfcBeam')
-
 print(len(things))
 
 #windowResult = windowRule.checkRule(model)
