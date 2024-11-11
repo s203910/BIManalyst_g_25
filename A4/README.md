1d27a4343388520e49638b22d86b8bd6ee9ae8b8
s214272 <s214272@dtu.dk>
2024-11-11 13:40:33 +0100

    Commit this

--- A4/README.md
+++ A4/README.md
@@ -1,2 +1,20 @@
+<<<<<<< Updated upstream
 # **Getting into Github**
 Using Github Desktop simplifies the process of working together in Git, as GUI platform helps beginners visualise the steps of pushing and pulling repositories. Quickly getting familiar with this tool is crucial for aspiring groups of coorporating BIM-gurus.
+=======
+# **Getting into Github Desktop**
+
+
+1. Visit the download page for [GitHub Desktop](https://github.com/apps/desktop)
+
+2. Click Download for Windows.
+![alt text](https://github.com/s203910/BIManalyst_g_25/blob/main/A4/Download%20button.png "Download Button")
+
+3. In your computer's Downloads folder, double-click the GitHub Desktop setup file or press the open file button in your webbrowser.
+![alt text](https://github.com/s203910/BIManalyst_g_25/blob/main/A4/Press_exe.png "Program now downloading")
+
+4. GitHub Desktop will now launch after downloading.
+![alt text](https://github.com/s203910/BIManalyst_g_25/blob/main/A4/image.png "Github Desktop now ready to use!")
+
+
+>>>>>>> Stashed changes
--- main.py
+++ main.py
@@ -9,9 +9,6 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_directory, 'rules'))
 
 # Henter funktionen fra mappen
-from rules.checkRule import elementRule
-from rules.numberElementCheck import amountElement
-from rules.nameElement import specifikElement
 from A3.assign_properties import assign_properties
 from A3.analyze_cross_section import analyze_cross_sections
 
@@ -24,19 +21,9 @@ model_filename = 'CES_BLD_24_06_STR.ifc'
 # Saml hele stien til IFC-filen
 model_path = os.path.join(models_directory, model_filename)
 
-# Åbn modellen
-model = ifcopenshell.open(model_path)
-
-import ifcopenshell
-
-# Åbn IFC-filen
-model = ifcopenshell.open(model_path)
-
-
+"""
 # Kald funktionen
 cross_section_overview = analyze_cross_sections(model_path)
-
-
 """
 
 cross_sections = [
@@ -47,25 +34,3 @@ cross_sections = [
 # Kald funktionen
 assign_properties(model_path, cross_sections)
 
-
-
-# Which elements needs to be checked
-element = 'IfcBeam'
-
-# Specifik beam type
-elementName = 'D22-400'
-
- Kan du evt. tage med hvis du synes
-# Amount required
-beams_required = 565
-
-# Check for amount of beams
-elementRule(model, element)
-
-# Check if the amount is same as the required amount
-amountElement(model, element, beams_required)
-
-
-# Check specifik element
-specifikElement(model)
-"""
